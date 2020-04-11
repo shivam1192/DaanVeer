40b5c2c3f7cf7f6b30e66f337479149cc23493f0
shivam1192 <sg9827252555@gmail.com>
2020-04-11 23:54:36 +0530

    popup for blockchain added

--- frontend/src/Component/Allblog.js
+++ frontend/src/Component/Allblog.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
-import {Card,CardActionArea,CardContent,Typography,Button,makeStyles,CardActions} from '@material-ui/core'
+import {Card,CardActionArea,CardContent,Typography,Button,makeStyles,CardActions,withStyles} from '@material-ui/core'
 import Blogauthor from '../Structure/Blogauthor'
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+
+
 const useStyles = makeStyles({
-    root: {
+    roots: {
       width: 340,
       border: "1px solid blue"
     },
@@ -13,13 +22,59 @@ const useStyles = makeStyles({
         flexWrap:'wrap',
         justifyContent:'space-between',
         alignItems:'center'
-    }
+    },
+    
   });
+  
+const styles = (theme) => ({
+  
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+
+  const DialogTitle = withStyles(styles)((props) => {
+    const { children, classes, onClose, ...other } = props;
+    return (
+      <MuiDialogTitle disableTypography className={classes.root} {...other}>
+        <Typography variant="h6">{children}</Typography>
+        {onClose ? (
+          <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </MuiDialogTitle>
+    );
+  });
+  
+  const DialogContent = withStyles((theme) => ({
+    root: {
+      padding: theme.spacing(2),
+    },
+  }))(MuiDialogContent);
+  
+  const DialogActions = withStyles((theme) => ({
+    root: {
+      margin: 0,
+      padding: theme.spacing(1),
+    },
+  }))(MuiDialogActions);
+  
 const Blog = () => {
     const [Blog,setBlog] = useState([])
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
 
-
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+    const handleClose = () => {
+      setOpen(false);
+    };
     useEffect(()=>{
         const getdata = async() =>{
             Axios.get("http://localhost:4000/blog").then((res)=>{
@@ -44,10 +99,11 @@ const Blog = () => {
     // }
 if(Blog.length!=0){
        return(
+           <div className = {classes.root}>
            <div className={classes.container}>
                { Blog.map((i)=>{
         return(
-        <Card className={classes.root}>
+        <Card className={classes.roots}>
         <CardActionArea>
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
@@ -71,12 +127,37 @@ if(Blog.length!=0){
           <Button size="small" color="primary" className="classes.button">
             Amount Raised
           </Button>
-          <Button size="small" color="primary" className="classes.button">
+          <Button size="small" color="primary" className="classes.button" onClick={handleClickOpen}>
                 Donate
           </Button>
+          <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Modal title
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            {i.Blog_wallet}
+          </Typography>
+          <Typography gutterBottom>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+            lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </Typography>
+          <Typography gutterBottom>
+            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
+            scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
+            auctor fringilla.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Save changes
+          </Button>
+        </DialogActions>
+      </Dialog>
         </CardActions>
       </Card>)
     })}
+               </div>
                </div>
        )
 }else{
